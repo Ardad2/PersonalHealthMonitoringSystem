bdcd4167f62f6788c331917b123358bad15be612
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-09 13:52:06 -0700

    creating the ui first

--- CSE335Homework1/CSE335Homework1/ContentView.swift
+++ CSE335Homework1/CSE335Homework1/ContentView.swift
@@ -7,13 +7,29 @@
 
 import SwiftUI
 
+extension Date {
+    var dayAfter: Date {
+        return Calendar.current.date(byAdding: .day, value: 1, to: self)!
+    }
+
+    var dayBefore: Date {
+        return Calendar.current.date(byAdding: .day, value: -1, to: self)!
+    }
+}
+
+
 struct ContentView: View {
+    
+    
+
     var body: some View {
+        
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text(Date.now, format: .dateTime.day().month().year())
+            Text(Date, format: .dateTime.day().month().year())
 
         }
         .padding()
