dc6d1999b92571a213a5b532061a4d201271785e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-08 20:07:38 -0700

    add proejct

---
+++ CSE335Homework1/CSE335Homework1/CSE335Homework1App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE335Homework1App.swift
+//  CSE335Homework1
+//
+//  Created by Arjun Dadhwal on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE335Homework1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
