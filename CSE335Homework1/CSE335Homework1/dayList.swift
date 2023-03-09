861f909c2a1a241ef3e4374fcdf90e086daef09d
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-09 13:56:19 -0700

    creating a list for storing day details

---
+++ CSE335Homework1/CSE335Homework1/dayList.swift
@@ -0,0 +1,19 @@
+//
+//  dayList.swift
+//  CSE335Homework1
+//
+//  Created by Arjun Dadhwal on 3/9/23.
+//
+
+import Foundation
+
+class dayList: ObservableObject
+{
+    @Published var queue:[dayRecord] = [dayRecord]();
+    
+    init() {
+        
+    }
+    
+    
+}
