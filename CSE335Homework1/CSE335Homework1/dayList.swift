a447a4ae97d245e526166fb5b1fdad0ef0ce4169
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-09 14:12:05 -0700

    created logic for adding day detail

--- CSE335Homework1/CSE335Homework1/dayList.swift
+++ CSE335Homework1/CSE335Homework1/dayList.swift
@@ -15,9 +15,37 @@ class dayList: ObservableObject
         
     }
     
-    func add_day()
+    func add_day(_ givenDate:Date, _ bloodPressureSystolic:Double, _ bloodPressureDiastolic:Double, _ sugarLevel:Double, _ symptoms:String)
     {
+        if (list.count >= 1)
+        {
+            if (list[0].get_date() == Date.now)
+            {
+                list[0].change_record(newBPSystolic:bloodPressureSystolic, newBPDiastolic:bloodPressureDiastolic, newSugar:sugarLevel, newSymptoms:symptoms)
+
+                
+                
+            }
+            else
+            {
+                list.insert(dayRecord(bloodPressureSystolic:bloodPressureSystolic, bloodPressureDiastolic:bloodPressureDiastolic, sugarLevel:sugarLevel, symptoms:symptoms), at: 0)
+                
+                if (list.count > 7)
+                {
+                    list.remove(at: 7);
+                }
+            }
+        }
         
+        //If there is nothing else on the list
+        
+        
+        else {
+            list.append(dayRecord(bloodPressureSystolic:bloodPressureSystolic, bloodPressureDiastolic:bloodPressureDiastolic, sugarLevel:sugarLevel, symptoms:symptoms))
+        }
     }
+    
+    
+    
     
 }
