0f41e569d4f4d2129ddf077f8ab7e660388684ab
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-08 20:21:51 -0700

    created the record for daily measurments

--- CSE335Homework1/CSE335Homework1/dayRecord.swift
+++ CSE335Homework1/CSE335Homework1/dayRecord.swift
@@ -9,14 +9,19 @@ import Foundation
 
 class dayRecord
 {
-    var date:Date? = nil
-    var bloodPressure:Double? = nil
-    var sugarLevel:Double? = nil
+    var date:Date? = nil;
+    var bloodPressureSystolic:Double? = nil;
+    var bloodPressureDiastolic:Double? = nil;
+    var sugarLevel:Double? = nil;
     var symptoms:String? = nil;
     
-    init ()
+    init (bloodPressureSystolic:Double, bloodPressureDiastolic:Double, sugarLevel:Double, symptoms:String)
     {
-        
+        self.date = Date.now;
+        self.bloodPressureSystolic = bloodPressureSystolic;
+        self.bloodPressureDiastolic = bloodPressureSystolic;
+        self.sugarLevel = sugarLevel;
+        self.symptoms = symptoms;
     }
 }
 
