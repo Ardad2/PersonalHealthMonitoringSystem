f09a0b0a8a6d0ffc5a9ccb736099b96b6d795f82
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-03-08 20:16:29 -0700

    create model for storing daily data

---
+++ CSE335Homework1/CSE335Homework1/dayRecord.swift
@@ -0,0 +1,23 @@
+//
+//  dayRecord.swift
+//  CSE335Homework1
+//
+//  Created by Arjun Dadhwal on 3/8/23.
+//
+
+import Foundation
+
+class dayRecord
+{
+    var date:Date? = nil
+    var bloodPressure:Double? = nil
+    var sugarLevel:Double? = nil
+    var symptoms:String? = nil;
+    
+    init ()
+    {
+        
+    }
+}
+
+
